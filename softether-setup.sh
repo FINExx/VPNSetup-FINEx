9e2eb39c20ba8e5b04872ddc4c1b30426ad6eb49
DevGracyy <dev.gracyy.002@gmail.com>
2018-04-23 01:15:30 +0800

    add SE

---
+++ softether-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Created by https://HostingTermurah.net
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+if [ ! -e /usr/bin/curl ]; then
+    apt-get -y update && apt-get -y upgrade
+	apt-get -y install wget curl
+fi
+clear
+CEK=`curl -s http://api.vps-murah.net/api/checker.php?mode=trial`;
+if [ "$CEK" != "MEMBER" ]; then
+		echo -e "${red}Permission Denied!${NC}";
+        echo $CEK;
+        exit 0;
+else
+echo -e "${green}Permission Accepted...${NC}"
+sleep 1
+clear
+fi
+mkdir -p /var/cache/data/important/bash/script/secure/softether
+echo " " > /var/cache/data/important/bash/script/secure/softether/softether.sh
+wget -qO /var/cache/data/important/bash/script/secure/softether/softether.sh http://script.hostingtermurah.net/autoscript/kvm/debian/debian7-kvm-softether.sh
+chmod +x /var/cache/data/important/bash/script/secure/softether/softether.sh
+bash /var/cache/data/important/bash/script/secure/softether/softether.sh
+rm -f /var/cache/data/important/bash/script/secure/softether/softether.sh
